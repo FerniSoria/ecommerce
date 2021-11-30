00f92af8185dcf3fae2b1e971ed902f692807d19
FerniSoria <87145192+FerniSoria@users.noreply.github.com>
2021-11-30 00:38:30 -0300

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+
+const Navbar = () => {
+    return (
+        <div className="container">
+  <nav className="navbar navbar-dark bg-primary">
+    <div className="container-fluid">
+      <p className="navbar-brand">Navbar F.S</p>
+    </div>
+  </nav>
+</div>
+
+        
+    )
+
+}
+
+export default Navbar
